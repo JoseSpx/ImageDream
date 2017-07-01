c8e0d31aee8d0416e923e9a927a9f2ffe8662e35
Jose Suarez <jose_95sp@hotmail.com>
2017-07-01 18:24:41 -0500

    Begin of the Project ImageDream

---
+++ src/imagedream/ImageDream.java
@@ -0,0 +1,11 @@
+package imagedream;
+
+import view.frmPrincipal;
+
+public class ImageDream {
+
+    public static void main(String[] args) {
+        new frmPrincipal().setVisible(true);
+    }
+    
+}
---
+++ src/view/frmPrincipal.java
@@ -0,0 +1,32 @@
+package view;
+public class frmPrincipal extends javax.swing.JFrame {
+
+    public frmPrincipal() {
+        initComponents();
+        setLocationRelativeTo(null);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1280, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 685, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
