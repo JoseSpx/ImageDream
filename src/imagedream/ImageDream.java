3f0f8ba20199f0caedb2d905dad026346813aa0a
Jose Suarez <jose_95sp@hotmail.com>
2017-07-02 16:51:39 -0500

    se realizo el funcionamiento de la opcion cerrar en el frmPrincipal

--- src/imagedream/ImageDream.java
+++ src/imagedream/ImageDream.java
@@ -1,16 +1,20 @@
 package imagedream;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UnsupportedLookAndFeelException;
+import view.frmPrincipal;
 import view.frmStart;
 
 public class ImageDream {
 
-    public static void main(String[] args) {
-        /*try {
-            new frmPrincipal().setVisible(true);
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        try {
+            new frmPrincipal("C:\\Users\\Jose sp\\Pictures\\usher.jpg").setVisible(true);
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
             Logger.getLogger(ImageDream.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
-        new frmStart().setVisible(true);
+        }
+        //new frmStart().setVisible(true);
     }
     
 }
--- src/view/frmPrincipal.java
+++ src/view/frmPrincipal.java
@@ -1,6 +1,6 @@
 package view;
 
-import javax.swing.UIManager;
+import javax.swing.JOptionPane;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class frmPrincipal extends javax.swing.JFrame {
@@ -9,9 +9,6 @@ public class frmPrincipal extends javax.swing.JFrame {
     
     public frmPrincipal(String path) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         initComponents();
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        //setSize(1300, 709);
-        //setResizable(false);
         setLocationRelativeTo(null);
         this.pathImage = path;
     }
@@ -82,35 +79,49 @@ public class frmPrincipal extends javax.swing.JFrame {
         jPanel1.add(jPanel3);
         jPanel3.setBounds(40, 30, 820, 640);
 
-        menuBarPrincipal.setBackground(new java.awt.Color(38, 50, 56));
+        menuBarPrincipal.setBackground(new java.awt.Color(102, 102, 102));
         menuBarPrincipal.setBorder(null);
-        menuBarPrincipal.setForeground(new java.awt.Color(51, 51, 255));
         menuBarPrincipal.setFocusCycleRoot(true);
         menuBarPrincipal.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
         menuBarPrincipal.setMaximumSize(new java.awt.Dimension(98, 300));
         menuBarPrincipal.setPreferredSize(new java.awt.Dimension(300, 19));
 
+        menuFile.setBackground(new java.awt.Color(102, 102, 102));
         menuFile.setForeground(new java.awt.Color(255, 255, 255));
         menuFile.setText("Archivo");
+        menuFile.setBorderPainted(true);
         menuFile.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+        menuFile.setOpaque(true);
 
-        itemFileOpen.setBackground(new java.awt.Color(96, 125, 139));
+        itemFileOpen.setBackground(new java.awt.Color(84, 110, 122));
         itemFileOpen.setForeground(new java.awt.Color(255, 255, 255));
         itemFileOpen.setText("Abrir");
+        itemFileOpen.setBorderPainted(true);
+        itemFileOpen.setOpaque(true);
         menuFile.add(itemFileOpen);
 
-        itemFileSaveAs.setBackground(new java.awt.Color(96, 125, 139));
+        itemFileSaveAs.setBackground(new java.awt.Color(84, 110, 122));
         itemFileSaveAs.setForeground(new java.awt.Color(255, 255, 255));
         itemFileSaveAs.setText("Guardar como");
+        itemFileSaveAs.setBorderPainted(true);
+        itemFileSaveAs.setOpaque(true);
         menuFile.add(itemFileSaveAs);
 
-        itemFileClose.setBackground(new java.awt.Color(96, 125, 139));
+        itemFileClose.setBackground(new java.awt.Color(84, 110, 122));
         itemFileClose.setForeground(new java.awt.Color(255, 255, 255));
         itemFileClose.setText("Cerrar");
+        itemFileClose.setBorderPainted(true);
+        itemFileClose.setOpaque(true);
+        itemFileClose.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemFileCloseActionPerformed(evt);
+            }
+        });
         menuFile.add(itemFileClose);
 
         menuBarPrincipal.add(menuFile);
 
+        jMenu2.setBackground(new java.awt.Color(102, 102, 102));
         jMenu2.setForeground(new java.awt.Color(255, 255, 255));
         jMenu2.setText("Seleccion");
         jMenu2.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
@@ -133,6 +144,17 @@ public class frmPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void itemFileCloseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemFileCloseActionPerformed
+        int option = JOptionPane.showConfirmDialog(null,
+                "se perdera todo lo aplicado en la imagen, esta seguro ?","Salir",JOptionPane.YES_NO_OPTION);
+        
+        if (option == JOptionPane.YES_OPTION){
+            new frmStart().setVisible(true);
+            this.dispose();
+        }
+        
+    }//GEN-LAST:event_itemFileCloseActionPerformed
 
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
