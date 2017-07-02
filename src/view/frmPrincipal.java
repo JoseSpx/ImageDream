07a714bc53b5ddc94e0824551297c38823a6a91f
Jose Suarez <jose_95sp@hotmail.com>
2017-07-02 01:47:29 -0500

    se agrego a la interfaz principal todos los paneles peor aun vacios

--- src/view/frmPrincipal.java
+++ src/view/frmPrincipal.java
@@ -21,11 +21,15 @@ public class frmPrincipal extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        lblOriginalImage = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        lblImageActual = new javax.swing.JLabel();
         menuBarPrincipal = new javax.swing.JMenuBar();
-        menuArchivo = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
+        menuFile = new javax.swing.JMenu();
+        itemFileOpen = new javax.swing.JMenuItem();
+        itemFileSaveAs = new javax.swing.JMenuItem();
+        itemFileClose = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -38,6 +42,45 @@ public class frmPrincipal extends javax.swing.JFrame {
         jPanel1.add(jLabel1);
         jLabel1.setBounds(895, 10, 1, 660);
 
+        lblOriginalImage.setBackground(new java.awt.Color(55, 71, 79));
+        lblOriginalImage.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
+        lblOriginalImage.setOpaque(true);
+        jPanel1.add(lblOriginalImage);
+        lblOriginalImage.setBounds(920, 30, 350, 180);
+
+        jPanel2.setBackground(new java.awt.Color(38, 50, 56));
+        jPanel2.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 348, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 418, Short.MAX_VALUE)
+        );
+
+        jPanel1.add(jPanel2);
+        jPanel2.setBounds(920, 250, 350, 420);
+
+        jPanel3.setBackground(new java.awt.Color(69, 90, 100));
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblImageActual, javax.swing.GroupLayout.DEFAULT_SIZE, 820, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblImageActual, javax.swing.GroupLayout.DEFAULT_SIZE, 640, Short.MAX_VALUE)
+        );
+
+        jPanel1.add(jPanel3);
+        jPanel3.setBounds(40, 30, 820, 640);
+
         menuBarPrincipal.setBackground(new java.awt.Color(38, 50, 56));
         menuBarPrincipal.setBorder(null);
         menuBarPrincipal.setForeground(new java.awt.Color(51, 51, 255));
@@ -46,46 +89,46 @@ public class frmPrincipal extends javax.swing.JFrame {
         menuBarPrincipal.setMaximumSize(new java.awt.Dimension(98, 300));
         menuBarPrincipal.setPreferredSize(new java.awt.Dimension(300, 19));
 
-        menuArchivo.setForeground(new java.awt.Color(255, 255, 255));
-        menuArchivo.setText("Archivo");
-        menuArchivo.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
-        menuArchivo.addFocusListener(new java.awt.event.FocusAdapter() {
+        menuFile.setForeground(new java.awt.Color(255, 255, 255));
+        menuFile.setText("Archivo");
+        menuFile.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+        menuFile.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
-                menuArchivoFocusGained(evt);
+                menuFileFocusGained(evt);
             }
         });
-        menuArchivo.addMouseListener(new java.awt.event.MouseAdapter() {
+        menuFile.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                menuArchivoMouseEntered(evt);
+                menuFileMouseEntered(evt);
             }
         });
 
-        jMenuItem1.setBackground(new java.awt.Color(96, 125, 139));
-        jMenuItem1.setForeground(new java.awt.Color(255, 255, 255));
-        jMenuItem1.setText("Abrir");
-        jMenuItem1.addFocusListener(new java.awt.event.FocusAdapter() {
+        itemFileOpen.setBackground(new java.awt.Color(96, 125, 139));
+        itemFileOpen.setForeground(new java.awt.Color(255, 255, 255));
+        itemFileOpen.setText("Abrir");
+        itemFileOpen.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
-                jMenuItem1FocusGained(evt);
+                itemFileOpenFocusGained(evt);
             }
         });
-        jMenuItem1.addMouseListener(new java.awt.event.MouseAdapter() {
+        itemFileOpen.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                jMenuItem1MouseEntered(evt);
+                itemFileOpenMouseEntered(evt);
             }
         });
-        menuArchivo.add(jMenuItem1);
+        menuFile.add(itemFileOpen);
 
-        jMenuItem2.setBackground(new java.awt.Color(96, 125, 139));
-        jMenuItem2.setForeground(new java.awt.Color(255, 255, 255));
-        jMenuItem2.setText("Guardar como");
-        menuArchivo.add(jMenuItem2);
+        itemFileSaveAs.setBackground(new java.awt.Color(96, 125, 139));
+        itemFileSaveAs.setForeground(new java.awt.Color(255, 255, 255));
+        itemFileSaveAs.setText("Guardar como");
+        menuFile.add(itemFileSaveAs);
 
-        jMenuItem3.setBackground(new java.awt.Color(96, 125, 139));
-        jMenuItem3.setForeground(new java.awt.Color(255, 255, 255));
-        jMenuItem3.setText("Salir");
-        menuArchivo.add(jMenuItem3);
+        itemFileClose.setBackground(new java.awt.Color(96, 125, 139));
+        itemFileClose.setForeground(new java.awt.Color(255, 255, 255));
+        itemFileClose.setText("Cerrar");
+        menuFile.add(itemFileClose);
 
-        menuBarPrincipal.add(menuArchivo);
+        menuBarPrincipal.add(menuFile);
 
         jMenu2.setForeground(new java.awt.Color(255, 255, 255));
         jMenu2.setText("Seleccion");
@@ -110,31 +153,35 @@ public class frmPrincipal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItem1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jMenuItem1FocusGained
+    private void itemFileOpenFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_itemFileOpenFocusGained
         
-    }//GEN-LAST:event_jMenuItem1FocusGained
+    }//GEN-LAST:event_itemFileOpenFocusGained
 
-    private void menuArchivoFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_menuArchivoFocusGained
+    private void menuFileFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_menuFileFocusGained
        
-    }//GEN-LAST:event_menuArchivoFocusGained
+    }//GEN-LAST:event_menuFileFocusGained
 
-    private void menuArchivoMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuArchivoMouseEntered
+    private void menuFileMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuFileMouseEntered
 
-    }//GEN-LAST:event_menuArchivoMouseEntered
+    }//GEN-LAST:event_menuFileMouseEntered
 
-    private void jMenuItem1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItem1MouseEntered
+    private void itemFileOpenMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_itemFileOpenMouseEntered
 
-    }//GEN-LAST:event_jMenuItem1MouseEntered
+    }//GEN-LAST:event_itemFileOpenMouseEntered
 
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem itemFileClose;
+    private javax.swing.JMenuItem itemFileOpen;
+    private javax.swing.JMenuItem itemFileSaveAs;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JMenu menuArchivo;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JLabel lblImageActual;
+    private javax.swing.JLabel lblOriginalImage;
     private javax.swing.JMenuBar menuBarPrincipal;
+    private javax.swing.JMenu menuFile;
     // End of variables declaration//GEN-END:variables
 }
