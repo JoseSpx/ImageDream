eb09b9fa5eaf17a1d13d4ba6a1a0d34ab3481849
Jose Suarez <jose95sp@outlook.com>
2017-07-23 05:30:44 -0500

    sumar imagenes temrinado con amabs opciones de extremo izquierdo y centrado

--- src/model/AddImage.java
+++ src/model/AddImage.java
@@ -25,65 +25,139 @@ public class AddImage {
     public BufferedImage add(){
         
         if(frmPrincipal.modeRGB){
-            BufferedImage imageActual = frmPrincipal.bufferedActualImage;
-        
-            int widthImageActual = imageActual.getWidth();
-            int heightImageActual = imageActual.getHeight();
-        
-            int widthImageToAdd = bufferedImageToAdd.getWidth();
-            int heightImageToAdd = bufferedImageToAdd.getHeight();
-        
-            int width, height;
-        
-            if(widthImageActual >= widthImageToAdd){
-                width = widthImageActual;
-            }else{
-                width = widthImageToAdd;
-            }
-        
-            if(heightImageActual >= heightImageToAdd){
-                height = heightImageActual;
-            }else{
-                height = heightImageToAdd;
-            }
+            
+            if(frmPrincipal.addImageExtremeLeft){
+                BufferedImage imageActual = frmPrincipal.bufferedActualImage;
 
-            BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                int widthImageActual = imageActual.getWidth();
+                int heightImageActual = imageActual.getHeight();
 
-            //lo rellenamos todo de negro
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    newImage.setRGB(j, i, new Color(0, 0, 0).getRGB());
+                int widthImageToAdd = bufferedImageToAdd.getWidth();
+                int heightImageToAdd = bufferedImageToAdd.getHeight();
+
+                int width, height;
+
+                if(widthImageActual >= widthImageToAdd){
+                    width = widthImageActual;
+                }else{
+                    width = widthImageToAdd;
                 }
-            }
 
-            //lo rrelenamos con la primera imagen
-            for (int i = 0; i < heightImageActual; i++) {
-                for (int j = 0; j < widthImageActual; j++) {
-                    newImage.setRGB(j, i,imageActual.getRGB(j, i));
+                if(heightImageActual >= heightImageToAdd){
+                    height = heightImageActual;
+                }else{
+                    height = heightImageToAdd;
                 }
-            }
 
-            //sumamos la segunda imagen por promedio
-            Color actual;
-            Color add;
-            for (int i = 0; i < heightImageToAdd; i++) {
-                for (int j = 0; j < widthImageToAdd; j++) {
-                    if(i < heightImageActual && j < widthImageActual){
-                        actual = new Color(imageActual.getRGB(j, i));
-                        add = new Color(bufferedImageToAdd.getRGB(j, i));
-                        //average = ( imageActual.getRGB(j, i) + bufferedImageToAdd.getRGB(j, i) ) / 2;
-                        newImage.setRGB(j, i,new Color((actual.getRed() + add.getRed()) / 2,
-                                                       (actual.getGreen()+ add.getGreen()) / 2,
-                                                       (actual.getBlue()+ add.getBlue()) / 2).getRGB());
+                BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+                //lo rellenamos todo de negro
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        newImage.setRGB(j, i, new Color(0, 0, 0).getRGB());
                     }
-                    else{
-                        newImage.setRGB(j, i,bufferedImageToAdd.getRGB(j, i));
-                    }
-
                 }
-            }
 
-            return newImage;
+                //lo rrelenamos con la primera imagen
+                for (int i = 0; i < heightImageActual; i++) {
+                    for (int j = 0; j < widthImageActual; j++) {
+                        newImage.setRGB(j, i,imageActual.getRGB(j, i));
+                    }
+                }
+
+                //sumamos la segunda imagen por promedio
+                Color actual;
+                Color add;
+                for (int i = 0; i < heightImageToAdd; i++) {
+                    for (int j = 0; j < widthImageToAdd; j++) {
+                        if(i < heightImageActual && j < widthImageActual){
+                            actual = new Color(imageActual.getRGB(j, i));
+                            add = new Color(bufferedImageToAdd.getRGB(j, i));
+                            //average = ( imageActual.getRGB(j, i) + bufferedImageToAdd.getRGB(j, i) ) / 2;
+                            newImage.setRGB(j, i,new Color((actual.getRed() + add.getRed()) / 2,
+                                                           (actual.getGreen()+ add.getGreen()) / 2,
+                                                           (actual.getBlue()+ add.getBlue()) / 2).getRGB());
+                        }
+                        else{
+                            newImage.setRGB(j, i,bufferedImageToAdd.getRGB(j, i));
+                        }
+
+                    }
+                }
+
+                return newImage;
+            }
+            else{System.out.println("centradooo");
+                BufferedImage imageActual = frmPrincipal.bufferedActualImage;
+        
+                int widthImageActual = imageActual.getWidth();
+                int heightImageActual = imageActual.getHeight();
+
+                int widthImageToAdd = bufferedImageToAdd.getWidth();
+                int heightImageToAdd = bufferedImageToAdd.getHeight();
+
+                int width, height;
+
+                if(widthImageActual >= widthImageToAdd){
+                    width = widthImageActual;
+                }else{
+                    width = widthImageToAdd;
+                }
+
+                if(heightImageActual >= heightImageToAdd){
+                    height = heightImageActual;
+                }else{
+                    height = heightImageToAdd;
+                }
+
+                BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+                //lo rellenamos todo de negro
+                for (int i = 0; i < height; i++) {
+                    for (int j = 0; j < width; j++) {
+                        newImage.setRGB(j, i, new Color(0, 0, 0).getRGB());
+                    }
+                }
+
+                int startX;
+                int startY;
+
+                startX = ( width - widthImageActual ) / 2;
+                startY = ( height - heightImageActual ) / 2;
+                
+                //lo rrelenamos con la primera imagen
+                for (int i = 0; i < heightImageActual; i++) {
+                    for (int j = 0; j < widthImageActual; j++) {
+                        newImage.setRGB(j + startX, i + startY,imageActual.getRGB(j, i));
+                    }
+                }
+
+                //sumamos la segunda imagen por promedio
+                startX = ( width - widthImageToAdd ) / 2;System.out.println("X : " + startX);
+                startY = ( height - heightImageToAdd ) / 2;System.out.println("Y : " + startY);
+                Color actual;
+                Color add;
+                for (int i = 0; i < heightImageToAdd ; i++) {
+                    for (int j = 0; j < widthImageToAdd ; j++) {
+                        if(i < (heightImageActual + startY) && j < (widthImageActual + startX)){
+                            actual = new Color(imageActual.getRGB(j + startX, i + startY));
+                            add = new Color(bufferedImageToAdd.getRGB(j, i));
+                            //average = ( imageActual.getRGB(j, i) + bufferedImageToAdd.getRGB(j, i) ) / 2;
+                            newImage.setRGB(j + startX, i + startY,new Color((actual.getRed() + add.getRed()) / 2,
+                                                           (actual.getGreen()+ add.getGreen()) / 2,
+                                                           (actual.getBlue()+ add.getBlue()) / 2).getRGB());
+                        }
+                        else{
+                            newImage.setRGB(j, i,bufferedImageToAdd.getRGB(j, i));
+                        }
+
+                    }
+                }
+
+                return newImage;
+            }
+            
+
         }
         else{
             
